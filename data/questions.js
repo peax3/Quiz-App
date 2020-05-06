8182faab91eee5e8beca8c7307f9099d57beef49
peax3 <peaceadjerese@gmail.com>
2020-05-06 22:03:00 +0100

    changed the questions

--- data/questions.js
+++ data/questions.js
@@ -1,18 +1,28 @@
 // array to hold the questions
 export default [
   {
-    question: "Which of these is the capital of Nigeria?",
-    options: ["Abuja", "Lagos", "Delta", "Ogun"],
+    question: "Which team won the 2018/2019 Premier League season?",
+    options: ["Manchester United", "Manchester City", "Liverpool", "Arsenal"],
+    answer: 1,
+  },
+  {
+    question: "Who is the Premier League all-time top goal scorer?",
+    options: ["Kun Aguero", "Wayne Rooney", "Thierry Henry", "Alan Shearer"],
+    answer: 3,
+  },
+  {
+    question: "In what season did Leicester City win the league?",
+    options: ["2015/16", "2016/17", "2017/18", "2018/2019"],
     answer: 0,
   },
   {
-    question: "When did Nigeria gets her independence?",
-    options: ["1900", "2000", "1960"],
+    question: "Which of these Teams was relegated in the 2017/18 season?",
+    options: ["Watford", "Burnley", "Fulham", "WestHam"],
     answer: 2,
   },
   {
-    question: "Which of these is the name of a planet",
-    options: ["Mecury", "saudi", "Delta"],
-    answer: 1,
+    question: "In what year did the Premier League begin?",
+    options: ["1990", "1991", "1992", "1993"],
+    answer: 2,
   },
 ];
